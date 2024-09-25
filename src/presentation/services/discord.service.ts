7ea903b46897158f119c7ed8baed09d88152f120
Jose Angel <ah@xdevelop.mx>
2024-09-24 20:38:05 -0600

    enviar gif

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -7,7 +7,14 @@ export class DiscordService {
 
   async notify(message: string) {
     const body = {
-      content: message
+      content: message,
+      embeds: [
+        {
+          image: {
+            url: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHA2OWo1bWd5bGYxZ2txOW1jcGdiZWVremVhYnpjOTZpcGgzNGNpYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1ykDbRCjniBVxwPK7C/giphy.gif"
+          }
+        }
+      ]
     }
 
     const response = await fetch(this.discordWebhookUrl, {
